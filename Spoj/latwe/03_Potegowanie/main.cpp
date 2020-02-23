8eaa4b3bd7028020f24e372e6f28ab12d2bd3e5e
MakaBresk <mikolaj240@interia.pl>
2020-02-23 16:57:10 +0100

    Przeniesienie kodu podprogramu obliczPotege na koniec kodu, usunięcie pętli zliczającej liczbę znaków do wystąpienia spacji

--- Spoj/latwe/03_Potegowanie/main.cpp
+++ Spoj/latwe/03_Potegowanie/main.cpp
@@ -7,6 +7,36 @@ using namespace std;
 int podstawaPotegi = 0;
 int wykladnikPotegi = 0;
 
+int obliczPotege(int podstawa, int wykladnik);
+
+int main()
+{
+    string line;
+
+    getline(cin, line); // Wczytywanie tekstu wierszami
+    int liczbaTestow = atoi(line.c_str()); // Konwersja string na int
+
+    while (liczbaTestow != 0)
+    {
+        getline(cin, line); // Wczytywanie tekstu wierszami
+        size_t pozycjaZnakuSpacji = line.find(' ');
+
+        // Przekopiowanie liczb znajdujących się w łańcuchu do osobnych zmiennych, a następnie konwersja string na int
+        string podstawaPotegi_str = line.substr(0, pozycjaZnakuSpacji);
+        podstawaPotegi = atoi(podstawaPotegi_str.c_str());
+
+        string wykladnikPotegi_str = line.substr(pozycjaZnakuSpacji+1,line.length());
+        wykladnikPotegi = atoi(wykladnikPotegi_str.c_str());
+
+        // % - reszta z dzielenia
+        int cyfraJednosciWyniku = obliczPotege(podstawaPotegi, wykladnikPotegi) % 10; // Wywołanie funkcji
+        cout << cyfraJednosciWyniku << endl;
+
+        liczbaTestow--;
+    }
+    return 0;
+}
+
 int obliczPotege(int podstawa, int wykladnik)
 {
     if (wykladnik == 1)
@@ -142,46 +172,4 @@ int obliczPotege(int podstawa, int wykladnik)
         break;
     }
     return wynik;
-}
-
-int main()
-{
-    string line;
-    getline(cin, line); // Wczytywanie tekstu wierszami
-
-    int liczbaTestow = atoi(line.c_str()); // Konwersja string na int
-    if (liczbaTestow >= 1 && liczbaTestow <= 10)
-    {
-        while (liczbaTestow != 0)
-        {
-            int ileZnakow = 0;
-
-            getline(cin, line); // Wczytywanie tekstu wierszami
-
-            // Zliczenie liczby znaków do spacji
-            for (int i = 0; i < line.length(); i++)
-            {
-                if (line[i] != ' ')
-                    ileZnakow++;
-                else
-                    break;
-            }
-
-            // Przekopiowanie liczb znajdujących się w łańcuchu do osobnych zmiennych, a następnie konwersja string na int
-            string podstawaPotegi_str = line.substr(0, ileZnakow);
-            podstawaPotegi = atoi(podstawaPotegi_str.c_str());
-
-            string wykladnikPotegi_str = line.substr(ileZnakow+1,line.length());
-            wykladnikPotegi = atoi(wykladnikPotegi_str.c_str());
-
-            // Sprawdzenie przedziału w jakim znajdują się podane liczby, a następnie wywołanie funkcji obliczającej potęgę
-            if ((podstawaPotegi >= 1 && podstawaPotegi <= 1000000000) && (wykladnikPotegi >= 1 && wykladnikPotegi <= 1000000000))
-            {
-                int cyfraJednosciWyniku = obliczPotege(podstawaPotegi, wykladnikPotegi) % 10;
-                cout << cyfraJednosciWyniku << endl;
-            }
-            liczbaTestow--;
-        }
-    }
-    return 0;
 }
